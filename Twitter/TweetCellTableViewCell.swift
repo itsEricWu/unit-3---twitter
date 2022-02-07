2447ba9c2652b0e86f1e7cae7dd17f4883695cdd
rriecc <s715860428@gmail.com>
2022-02-06 20:14:35 -0500

    add files
    unit 3 commit

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by 吴承翔 on 2/6/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    
+    @IBOutlet weak var profileimageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
